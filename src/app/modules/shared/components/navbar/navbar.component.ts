732be97329ff577c76cd37982e3b3b08c141080d
FraN212121 <franbpascua3@gmail.com>
2024-07-23 11:00:16 -0300

    utilizamos directivas ngif para el navegador e instalamos sweetalert2

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from 'src/app/modules/autentificacion/service/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+logueado = true; // booleana para manejo de registro y el inicio de sesion
+deslogueado = false; // booleana para manejon de cierre de sesion
 
+
+constructor(
+  public servicioAuth: AuthService,
+  public servicioRutas: Router
+){}
+
+// funcion "iniciar" para invertir los valores
+Iniciar(){
+  this.logueado=false;
+  this.deslogueado=true;
+}
+
+// funcion "cerrarsesion" devuelve a los valores originales
+Cerrarsesion(){
+  this.deslogueado=false;
+  this.logueado=true;
+
+  // llamamos al metodo "CerrarSesion" del auth.service para limpiar el token
+  this.servicioAuth.CerrarSesion();
+  this.servicioRutas.navigate(['/']);
+}
 }

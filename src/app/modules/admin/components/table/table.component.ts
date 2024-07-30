5c75e0e6ef93dbfe1a317e14b4d66cdb6c2d23e8
FraN212121 <franbpascua3@gmail.com>
2024-07-30 10:59:05 -0300

    creamos interfaz producto; creamos servicio CRUD; definimos estructura del componente 'table'; definimos formulario utilizando directivas de form para producto

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { Productos } from 'src/app/models/productos';
+import { CrudService } from '../../services/crud.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
+  // creamos collecion local de productos -> la definimos como array
+  collecionProductos: Productos[]=[];
 
+  // definimos formularios para los productos 
+  /* 
+  atributos alfanumericos (string) se inicializan con comillas simples
+  atributos numericos (number) se inicializan ocn cero (0)
+  */
+  producto = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    precio: new FormControl(0, Validators.required),
+    descripcion: new FormControl('', Validators.required),
+    categoria: new FormControl('', Validators.required),
+    imagen: new FormControl('', Validators.required),
+    alt: new FormControl('', Validators.required),
+  })
+
+  constructor(public servicioCrud:CrudService){}
 }
